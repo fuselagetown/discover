088c890220e9ef2fe8e1bdd62d369732c3ba0e4c
Torunn Seim Skrogstad <torunn@skrogstad.no>
2023-02-28 12:07:59 -0800

    smooth overlay animation

--- assets/js/torunn.js
+++ assets/js/torunn.js
@@ -6,12 +6,15 @@ $(function() {
 
   $(".carousel-item__actions--ratings--like").click(function() {
     $(this).closest( ".carousel-item" ).next(".similar-items").addClass('im-loading');
+    $(this).closest( ".carousel-item__actions--ratings" ).find(".carousel-item__actions--ratings--dislike").removeClass('active');
+    $(this).closest( ".carousel-item" ).removeClass("disliked");
     $(this).toggleClass("active");
     return false;
   });
 
 
   $(".carousel-item__actions--ratings--dislike").click(function() {
+    $(this).toggleClass("active");
     $(this).closest( ".carousel-item" ).addClass("disliked");
     $(this).closest( ".carousel-item__actions--ratings" ).find(".carousel-item__actions--ratings--like").removeClass('active');
     return false;
